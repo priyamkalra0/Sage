f03304ff8aec75134b1424f17aba302da8e3bd49
priyamkalra0 <priyamkalra0@gmail.com>
2026-01-11 06:25:45 +0300

    fs: impl `write_all_bytes()` helper

--- include/Filesystem.hpp
+++ include/Filesystem.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+#include <vector>
 #include <fstream>
 
 /* See: https://gist.github.com/matthewjberger/0b6760de2ed41436a19235cfbe1b2d31 */
@@ -18,4 +20,15 @@ inline void read_all_bytes(std::string const& path, std::vector<unsigned char>& 
         std::istream_iterator<unsigned char>(),
         std::back_inserter(out)
     );
+}
+
+inline void write_all_bytes(const std::string& path, unsigned char const* data, unsigned int const size) {
+    std::ofstream stream { path, std::ios::out | std::ios::binary | std::ios::trunc };
+    stream.write(reinterpret_cast<char const*>(data), size);
+    stream.close();
+}
+
+inline void write_all_bytes(std::string const& path, std::vector<unsigned char> const& data)
+{
+    write_all_bytes(path, data.data(), data.size());
 }
