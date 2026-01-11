cad37e4d7a721b8aac8757f1ba7f83e8402040fd
priyamkalra0 <priyamkalra0@gmail.com>
2026-01-11 06:47:04 +0300

    core/sav: made header-only

--- include/Core/Sav.hpp
+++ include/Core/Sav.hpp
@@ -4,12 +4,17 @@
 #include <vector>
 #include <string_view>
 #include <unordered_map>
+#include "Filesystem.hpp"
+#include "MurmurHash.hpp"
 #include "Types.hpp"
 
 class Sav
 {
 public:
-    explicit Sav(std::string const& path);
+    explicit Sav(std::string const& path) {
+        read_all_bytes(path, m_data);
+        populate_offsets(m_offsets);
+    };
 
     /* Get reference to value by name (uses cached offset) */
     template <typename T>
@@ -61,7 +66,31 @@ private:
         return reinterpret_cast<T*>(&m_data[0] + offset);
     }
 
-    void populate_offsets(std::unordered_map<std::string_view, u32>& out);
+    void populate_offsets(std::unordered_map<std::string_view, u32>& out) {
+        for (u32 offset = 0x00; offset < m_data.size(); offset += 0x08)
+        {
+            u32 hash = get<u32>(offset);
+            u32 const value_offset = offset + sizeof(u32);
+
+            /* Hashtable ends at MetaData.SaveTypeHash
+             * See: https://github.com/marcrobledo/savegame-editors/blob/b65dc1ecf655ba4f5f8bb74d4a7d402fc375fbf1/zelda-totk/zelda-totk.variables.js#L757
+             */
+            if (hash == 0xa3db7114) break;
+            if (!Hashes.count(hash)) continue;
+
+            auto const& name = Hashes.at(hash);
+            if (name.back() == '*') { // is reference
+                auto const& actual_name = name.substr(0, name.size() - 1);
+                out[actual_name] = get<u32>(value_offset);
+            } else { // is value
+                out[name] = value_offset;
+            }
+
+            /* Some offsets need to be manually shifted */
+            /* i.e I do not know a better way to do this */
+            if (name == "LocationMarker") out[name] += 0x30;
+        }
+    };
 
     std::vector<u8> m_data;
     std::unordered_map<std::string_view, u32> m_offsets;
